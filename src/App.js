a73d0505982d5948dd5be12fb500490bb465debd
vishal soni <vishsoni043@gmail.com>
2023-08-11 18:46:42 +0530

    created app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
-      <h1> MCR-TEN</h1>
+      <h1> MCR-10</h1>
     </div>
   );
 }

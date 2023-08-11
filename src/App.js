584c35deb67b442b36b15ff4d5ff77a50c3887cd
vishal soni <vishsoni043@gmail.com>
2023-08-11 18:55:39 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-
 function App() {
   return (
     <div className="App">
